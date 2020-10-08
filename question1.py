0045d8fe6324134903156055751725f8667d66ab
Alexander Rodrigues <alexander.rodrigues@daviestech.org>
2020-10-08 16:57:50 +0000

    submitting

---
+++ question1.py
@@ -0,0 +1,4 @@
+num = int(input("Enter a number: "))
+print(int(num + 1))
+print(int(num + 2))
+print(int(num + 3))
---
+++ question2.py
@@ -0,0 +1,5 @@
+num1 = float(input("Enter a decimal: "))
+num2 = float(input("Enter a decimal: "))
+num3 = float(input("Enter a decimal: "))
+
+print(float(num1 + num2 + num3))
